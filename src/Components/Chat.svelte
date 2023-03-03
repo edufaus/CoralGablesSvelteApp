ee0f8f7c967bd46add9adeceb24c02859210a56d
Eduard Faus <efaus25@belenwolverines.org>
2023-03-03 14:52:13 +0000

    Initial commit

---
+++ src/Components/Chat.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let chat = ""
+  export let from = "me"
+</script>
+{#if from == "bot"}
+<div class="chat chat-start">
+  <div class="chat-bubble">{chat}</div>
+</div>
+{:else}
+<div class="chat chat-end">
+  <div class="chat-bubble">{chat}</div>
+</div>
+{/if}
